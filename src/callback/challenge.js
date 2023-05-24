3199dedc6930eba3c4ef84508556701c796eb02a
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-05-24 14:20:36 -0600

    Fetch data

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,4 +1,5 @@
-const XMLHttpRequest = require('xmlhttprequest');
+// XMLHTTPRequest usage
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 const API = 'https://api.escuelajs.con/api/v1';
 
 function fetchData(urlApi, callback) {
@@ -9,11 +10,24 @@ function fetchData(urlApi, callback) {
         if (xhttp.readyState === 4) { // 4 stands for 'Completed' state (There are 5)
             if(xhttp.status === 200) { // 200 stands for 'OK' request has been correct
                 callback(null, JSON.parse(xhttp.responseText)); // Parsing receiving text into an object
+            } else {
+                const error = new Error('Error' + urlApi) // Error comes from an API's element
+                return callback(error, null);
             }
-        } else {
-            const error = new Error('Error' + urlApi) // Error comes from an API's element
-            return callback(error, null);
         }
     }
     xhttp.send(); // Send request
 }
+
+fetchData(`${API}/products`, function (error1, data1) { // get products
+    if (error1) return console.error(error1);
+    fetchData(`${API}/products/${data1[0].id}`, function (error2, data2) { // get first product's id
+        if (error2) return console.error(error2);
+        fetchData(`${API}/categories/${data2?.category?.id}`, function (error3, data3) { // optional chaining
+            if (error3) return console.error(error3);
+            console.log(data1[0]); // prints first object from products
+            console.log(data2.title); // prints its category
+            console.log(data3.name); // prints its name
+        });
+    });
+});
