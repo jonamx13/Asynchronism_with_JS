687148f5e6596d2b6cf6c9c02e27d595db69782e
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-05-25 13:37:51 -0600

    Exercise 2 - async promise function

--- exercices.js
+++ exercices.js
@@ -8,3 +8,17 @@ function execCallback(callback) {
 
 const myFunc = () => console.log('Log after 2s');
 execCallback(myFunc);
+
+//* Excercise 2
+
+delay(2000, "Hello after 2s")
+.then((message) => console.log(message));
+
+delay(3000, "Hello after 3s")
+.then((message) => console.log(message))
+
+function delay(time, message) {
+    return new Promise((resolve,reject) => {
+        setTimeout(()=> {console.log(message)}, time);
+    });
+}
