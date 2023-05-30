6e212826347a4d78002c88c0e6a5526d528d1722
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-05-30 14:48:49 -0600

    Exercise 3 - Try catch error

--- exercices.js
+++ exercices.js
@@ -11,6 +11,8 @@ execCallback(myFunc);
 
 //* Excercise 2
 
+//? Make a function that delays 2 and 3 seconds to print a message
+
 delay(2000, "Hello after 2s")
 .then((message) => console.log(message));
 
@@ -18,7 +20,25 @@ delay(3000, "Hello after 3s")
 .then((message) => console.log(message))
 
 function delay(time, message) {
-    return new Promise((resolve,reject) => {
+    return new Promise(() => {
         setTimeout(()=> {console.log(message)}, time);
     });
 }
+
+//* Exercise 3
+
+//? Throw an error from a function, calling an API that does not exist
+
+import fetch from 'node-fetch';
+
+async function runCode() {
+    const url = 'https://domain-api-com';
+
+    try {
+        await fetch(url)
+    } catch {
+        throw new Error('API not found')
+    }
+}
+
+runCode();
